03821799bd0b2fefecb53690410646a35c9b7aae
Polina Prokopenko <topolina1337@gmail.com>
2024-12-20 20:27:29 +0100

    last commit, but right

---
+++ README.md
@@ -0,0 +1,11 @@
+# Credit  
+
+## Card lengths and starting numbers  
+
+We'll consider that:
+
+| Card type  | Starting digits    | Length |  
+| ---------- | ------------------ | ------ |  
+| Visa       | 4                  | 16, 13 |
+| MasterCard | 51, 52, 53, 54, 55 | 16, 13 |
+| Amex       | 34, 37             | 15     |
--- credit.c
+++ credit.c
@@ -5,8 +5,8 @@
 int CheckCreditCard(long number); // TODO
 
 // utilities for number verification
-int CheckNumberLength(long number);                  // done
-int CheckNumberFirstDigits(long number, int length); // TODO
+int GetNumberLength(long number);        // done
+int CheckNumberFirstDigits(long number); // done
 
 // utilities for Luhn's algorithm
 int GetLastDigit(long number);                 // done
@@ -15,14 +15,11 @@ int GetChecksum(long number);                  // TODO
 
 int main(void)
 {
-  long visaNumberTest = 4003600000070314;
-  long randomNumber = 45645;
+  long visaNumberTest = 370360000000031;
 
-  int k = 4;
+  int isValid = CheckNumberFirstDigits(visaNumberTest);
 
-  printf("%li\n", (visaNumberTest / (long)pow(10, k)) % 10);
-  printf("%i\n", GetNDigit(visaNumberTest, 2));
-  printf("%i\n", GetLastDigit(visaNumberTest));
+  printf("%i\n", isValid);
 
   return 0;
 }
@@ -32,7 +29,7 @@ int CheckCreditCard(long number)
   return 0;
 }
 
-int CheckNumberLength(long number)
+int GetNumberLength(long number)
 {
   int length;
   if (number == 0)
@@ -43,14 +40,86 @@ int CheckNumberLength(long number)
   {
     if (number < 0)
       number = -number;
+
     length = (int)log10(number) + 1;
   }
 
   return length;
 }
 
-int CheckNumberFirstDigits(long number, int length)
+// 0 invalid, 1 visa, 2 mc, 3 amex
+int CheckNumberFirstDigits(long number)
 {
+  int cardType = 0;
+  int length = GetNumberLength(number);
+  int firstDigit;
+  int secondDigit;
+
+  if (length == 16) // visa or mc
+  {
+    firstDigit = GetNDigit(number, 15);
+    secondDigit = GetNDigit(number, 14);
+    if (firstDigit == 4)
+    {
+      return 1;
+    }
+    else if (firstDigit == 5)
+    {
+      if (secondDigit == 1 || secondDigit == 2 || secondDigit == 3 || secondDigit == 4 || secondDigit == 5)
+      {
+        return 2;
+      }
+      else
+      {
+        return 0;
+      }
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  else if (length == 15) // amex
+  {
+    firstDigit = GetNDigit(number, 14);
+    secondDigit = GetNDigit(number, 13);
+    if (firstDigit == 3 && (secondDigit == 4 || secondDigit == 7))
+    {
+      return 3;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  else if (length == 13) // visa or mc
+  {
+    firstDigit = GetNDigit(number, 12);
+    secondDigit = GetNDigit(number, 11);
+    if (firstDigit == 4)
+    {
+      return 1;
+    }
+    else if (firstDigit == 5)
+    {
+      if (secondDigit == 1 || secondDigit == 2 || secondDigit == 3 || secondDigit == 4 || secondDigit == 5)
+      {
+        return 2;
+      }
+      else
+      {
+        return 0;
+      }
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  else
+  {
+    return 0;
+  }
 }
 
 int GetLastDigit(long number)
