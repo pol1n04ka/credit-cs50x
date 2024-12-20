dfbd6103e0b0571a130b0237a38b5e48eec6d765
Polina Prokopenko <topolina1337@gmail.com>
2024-12-20 20:24:30 +0100

    first iteration of CheckNumberFirstDigits

--- credit.c
+++ credit.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-int CheckCreditCard(long cardNumber);
-int GetLastDigit(long number);
-int GetNDigit(long number, int digitPosition);
+// main function for verification card number
+int CheckCreditCard(long number); // TODO
+
+// utilities for number verification
+int CheckNumberLength(long number);                  // done
+int CheckNumberFirstDigits(long number, int length); // TODO
+
+// utilities for Luhn's algorithm
+int GetLastDigit(long number);                 // done
+int GetNDigit(long number, int digitPosition); // done
+int GetChecksum(long number);                  // TODO
 
 int main(void)
 {
   long visaNumberTest = 4003600000070314;
+  long randomNumber = 45645;
 
   int k = 4;
 
@@ -18,9 +27,30 @@ int main(void)
   return 0;
 }
 
-int CheckCreditCard(long cardNumber)
+int CheckCreditCard(long number)
 {
   return 0;
+}
+
+int CheckNumberLength(long number)
+{
+  int length;
+  if (number == 0)
+  {
+    length = 1;
+  }
+  else
+  {
+    if (number < 0)
+      number = -number;
+    length = (int)log10(number) + 1;
+  }
+
+  return length;
+}
+
+int CheckNumberFirstDigits(long number, int length)
+{
 }
 
 int GetLastDigit(long number)
@@ -28,7 +58,13 @@ int GetLastDigit(long number)
   return number % 10;
 }
 
+// get digit from requested position in a number
+// 0 is last digit, 1 is second from end and so on
 int GetNDigit(long number, int digitPosition)
 {
   return (number / (long)pow(10, digitPosition)) % 10;
+}
+
+int GetChecksum(long number)
+{
 }
