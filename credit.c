c463a315825009c41d9958778a659286d01c5967
Polina Prokopenko <topolina1337@gmail.com>
2024-12-21 20:12:42 +0100

    implemented essential functions for credit card verification

--- credit.c
+++ credit.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 
-// main function for verification card number
+// main function for verifying card number
+
 int CheckCreditCard(long number); // TODO
 
 // utilities for number verification
+
 int GetNumberLength(long number);        // done
 int CheckNumberFirstDigits(long number); // done
 
 // utilities for Luhn's algorithm
+
 int GetLastDigit(long number);                 // done
 int GetNDigit(long number, int digitPosition); // done
-int GetChecksum(long number);                  // TODO
+int GetChecksum(long number);                  // done, maybe need refactoring
+int VerifyChecksum(int checksum);              // done
 
 int main(void)
 {
-  long visaNumberTest = 373600000000314;
+  long visaNumberTest = 5115800601659182;
 
   int isValid = CheckNumberFirstDigits(visaNumberTest);
 
   printf("%i\n", isValid);
+
+  int checkSum = GetChecksum(visaNumberTest);
+
+  printf("%i\n", VerifyChecksum(checkSum));
 
   return 0;
 }
@@ -50,7 +58,6 @@ int GetNumberLength(long number)
 // 0 invalid, 1 visa, 2 mc, 3 amex
 int CheckNumberFirstDigits(long number)
 {
-  int cardType = 0;
   int length = GetNumberLength(number);
   int firstDigit = GetNDigit(number, length - 1);
   int secondDigit = GetNDigit(number, length - 2);
@@ -94,12 +101,13 @@ int CheckNumberFirstDigits(long number)
   }
 }
 
+// returns last digit in a number
 int GetLastDigit(long number)
 {
   return number % 10;
 }
 
-// get digit from requested position in a number
+// returns digit from requested position in a number
 // 0 is last digit, 1 is second from end and so on
 int GetNDigit(long number, int digitPosition)
 {
@@ -108,4 +116,50 @@ int GetNDigit(long number, int digitPosition)
 
 int GetChecksum(long number)
 {
+  int length = GetNumberLength(number);
+  int sumFirst = 0;
+  int sumSecond = 0;
+  int sumFinal;
+
+  int currentDigit;
+
+  for (int i = 1; i < length; i += 2)
+  {
+    currentDigit = GetNDigit(number, i);
+
+    if ((currentDigit * 2) > 9)
+    {
+      int a = GetNDigit(currentDigit * 2, 0);
+      int b = GetNDigit(currentDigit * 2, 1);
+
+      sumFirst += a + b;
+    }
+    else
+    {
+      sumFirst += currentDigit * 2;
+    }
+  }
+
+  for (int j = 0; j < length; j += 2)
+  {
+    currentDigit = GetNDigit(number, j);
+    sumSecond += currentDigit;
+  }
+
+  sumFinal = sumFirst + sumSecond;
+
+  return sumFinal;
+}
+
+// 0 invalid, 1 valid
+int VerifyChecksum(int checksum)
+{
+  if (GetLastDigit(checksum) == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
 }
